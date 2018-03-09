61479fae47e85c95d44c250a6fa4e06a5cb7c668
Miracle-qyd <1084176951@qq.com>
2018-03-09 17:34:30 +0800

    task

--- spring-aop-011/src/main/java/com/shsxt/aspectj/LogCut.java
+++ spring-aop-011/src/main/java/com/shsxt/aspectj/LogCut.java
@@ -5,27 +5,23 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
-@Aspect//标记aop
+@Aspect
 public class LogCut {
 
 	/**
 	 * 切点
 	 */
+//	@Pointcut("excution(* com.shsxt.service..*.*(..))")
 	@Pointcut("execution (* com.shsxt.service..*.*(..))")
-	public void cut() {
-	}
-
-	@Before(value = "cut ()")
-	public void before() {
-		System.out.println("前置通知。。。。");
-	}
-
-	/**
-	 * 返回通知
-	 */
-	@AfterReturning ("cut()")
-	public void afterReturning () {
-		System.out.println("返回通知");
+	public void cut(){}
+	
+	@Before("cut()")
+	public void before(){
+		System.out.println("前置通知");
 	}
 	
+	@AfterReturning("cut()")
+	public void afterReturning(){
+		System.out.println("返回通知。。。");
+	}
 }
---
+++ spring-aop-011/src/main/java/com/shsxt/service/UserService.java
@@ -0,0 +1,10 @@
+package com.shsxt.service;
+
+import org.aspectj.weaver.VersionedDataInputStream;
+
+public class UserService {
+
+	public void print(){
+		System.out.println("测试  aop 通知");
+	}
+}
